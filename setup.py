732a692e2990681b6b344e98fe0ae1458d195a2b
Vallentin <mail@vallentinsource.com>
2016-03-28 20:40:44 +0200

    Following Python coding conventions

--- setup.py
+++ setup.py
@@ -16,17 +16,17 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 setup(
-	name = "mount.py",
-	version = "1.0",
-	py_modules = [ "mount"],
-	author = "Christian Vallentin",
-	author_email = "mail@vallentinsource.com",
-	url = "https://github.com/MrVallentin/mount.py",
-	description = "Python only module for listing, mounting and unmounting media devices",
-	long_description = "mount.py is a simple, small and self-contained Python only module, for listing, mounting and unmounting media drives on Linux.",
-	keywords = "listing media devices mount mounting unmount unmounting",
-	license = "MIT",
-	classifiers = [
+	name="mount.py",
+	version="1.0",
+	py_modules=["mount"],
+	author="Christian Vallentin",
+	author_email="mail@vallentinsource.com",
+	url="https://github.com/MrVallentin/mount.py",
+	description="Python only module for listing, mounting and unmounting media devices",
+	long_description="mount.py is a simple, small and self-contained Python only module, for listing, mounting and unmounting media drives on Linux.",
+	keywords="listing media devices mount mounting unmount unmounting",
+	license="MIT",
+	classifiers=[
 		"Development Status :: 1 - Alpha",
 		"Intended Audience :: Developers",
 		"License :: OSI Approved :: MIT License",
